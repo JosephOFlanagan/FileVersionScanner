66bd61744db077446e2a64764495ff6febcc324d
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2018-09-04 13:59:32 +0000

    Update fmbPllConvert.java

--- fmbPllConvert.java
+++ fmbPllConvert.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class fmbPllConvert
 {
     //Class variables
-    String[] pvcsObjects;
+    String[] objects;
     List<String> fmbxmlArray;
     List<String> pldArray;
     int[] noOfFMBs;
@@ -20,7 +20,7 @@ public class fmbPllConvert
     //Class Constructor
     public fmbPllConvert(String[] obj, File dir, Config frm2)
     {
-        pvcsObjects = obj;
+        objects = obj;
         directory = dir;
         form2xml = new File(frm2.forms2xml);
     }
@@ -31,19 +31,19 @@ public class fmbPllConvert
         fmbxmlArray = new ArrayList<String>();
         pldArray = new ArrayList<String>();
         //For All Objects in PVCS Array
-        for (int i=0; i < pvcsObjects.length; i++)
+        for (int i=0; i < objects.length; i++)
         {
             //If File type is an FMB
-            if(("fmb").equals(pvcsObjects[i].substring((pvcsObjects[i].length() - 3))))
+            if(("fmb").equals(objects[i].substring((objects[i].length() - 3))))
             {
                 System.out.println("Checking forms and converting them into xml files");
-                File currentFile = new File(directory + "/" + pvcsObjects[i]);
-                String currentFileString = (directory + "\\" + pvcsObjects[i]);
+                File currentFile = new File(directory + "/" + objects[i]);
+                String currentFileString = (directory + "\\" + objects[i]);
                 //Run Forms2XML for that particular object
                 Runtime fmx = Runtime.getRuntime();
                 try
                 {
-                    System.out.println("Converting " + pvcsObjects[i] + " into an xml file");
+                    System.out.println("Converting " + objects[i] + " into an xml file");
                     //Process cd = fmx.exec("cd " + fmxldir);
                     String formdirString = form2xml.getParent();
                     File formdir = new File(formdirString);
@@ -53,7 +53,7 @@ public class fmbPllConvert
                     TimeUnit.SECONDS.sleep(10);
                     //Need to look in the directory for the file
                     File[] dir_contents = directory.listFiles();
-                    File xmlCheck = new File(directory + "/" + pvcsObjects[i].substring(0,(pvcsObjects[i].length() - 4)) + "_fmb.xml");
+                    File xmlCheck = new File(directory + "/" + objects[i].substring(0,(objects[i].length() - 4)) + "_fmb.xml");
                     boolean fileFound = false;
                     //Check to see if the xml has been created yet.
                     for (int k = 0; k < dir_contents.length; k++)
@@ -62,7 +62,7 @@ public class fmbPllConvert
                         if ((xmlCheck).equals(dir_contents[k]))
                         {
                             fileFound = true;
-                            fmbxmlArray.add(pvcsObjects[i].substring(0,(pvcsObjects[i].length() - 4)) + "_fmb.xml");
+                            fmbxmlArray.add(objects[i].substring(0,(objects[i].length() - 4)) + "_fmb.xml");
                             break;
                         }
                     }
@@ -85,16 +85,16 @@ public class fmbPllConvert
                 }
             }
             //Else IF File type is PLL
-            else if (("pll").equals(pvcsObjects[i].substring((pvcsObjects[i].length() - 3))))
+            else if (("pll").equals(objects[i].substring((objects[i].length() - 3))))
             {
                 System.out.println("Checking pll files and converting them into pld files");
                 Runtime plx = Runtime.getRuntime();
                 try
                 {
                     //Run frmcmp to convert the file into a pld
-                    File currentFile = new File(directory + "/" + pvcsObjects[i].substring(0,(pvcsObjects[i].length() - 3)) + "pld");
-                    System.out.println("Converting " + pvcsObjects[i] + " into a pld file");
-                    Process plxpr = plx.exec("frmcmp userid=drivedev/drivedev@drg1dev module=" + pvcsObjects[i] + " module_type=LIBRARY window_state=minimize script=YES", null, directory);
+                    File currentFile = new File(directory + "/" + objects[i].substring(0,(objects[i].length() - 3)) + "pld");
+                    System.out.println("Converting " + objects[i] + " into a pld file");
+                    Process plxpr = plx.exec("frmcmp userid=drivedev/drivedev@drg1dev module=" + objects[i] + " module_type=LIBRARY window_state=minimize script=YES", null, directory);
                     //frmcmp runs independently to the runtime of testPrep, so we need to force the program to wait until the pld has been created, otherwise the array will store null.
                     plxpr.waitFor();
                     //Need to look in the directory for the file
@@ -108,7 +108,7 @@ public class fmbPllConvert
                         {
                             //We don't need this for loop anymore for that particular object, so add it to the pllTextArray and let the program know it's been found.
                             fileFound = true;
-                            pldArray.add(pvcsObjects[i].substring(0,(pvcsObjects[i].length() - 3)) + "pld");
+                            pldArray.add(objects[i].substring(0,(objects[i].length() - 3)) + "pld");
                             //Exit out of this for loop due to finding the file
                             break;
                         }
