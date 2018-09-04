43eb5aaa4443957b526f12d04ac827e436962e9b
JosephOFlanagan <joseph.oflanagan@yahoo.co.uk>
2018-09-04 13:36:19 +0000

    Update fileCheck.java

--- fileCheck.java
+++ fileCheck.java
@@ -52,7 +52,7 @@ public class fileCheck
         }
         if (directoryExists == true)
         {
-            //For all files in responseDeskData.pvcsobjects
+            //For all files in fileData.Objects
             for (int i = 0; i < objectNames.length; i++)
             {
                 fileExists = false;
